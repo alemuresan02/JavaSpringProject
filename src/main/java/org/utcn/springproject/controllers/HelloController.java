adb52da0ea8f6d89420cd68eee079242cb9bd078
alemuresan <Muresan.Al.Alexandra@student.utcluj.ro>
2023-12-14 20:36:49 +0200

    Added Static Template

--- src/main/java/org/utcn/springproject/controllers/HelloController.java
+++ src/main/java/org/utcn/springproject/controllers/HelloController.java
@@ -4,47 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    // Handles request at localhost:8080/hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
-    // lives /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
-
-    // lives /hello/hello
-    // Handles request of the form /hello?name=UTCN
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
+    // Responds to /hello?name=UTCN
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    // Handles requests of the form /hello/UTCN
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    // lives /hello/form
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = 'hello' method = 'post'>" + //submit a request to /hello
-                "<input type = 'text' name = 'name'>" +
-                "<input type = 'submit' value = 'Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
 }
