4be996f73b78cf252a571f45d06edf71e6246cab
alemuresan <Muresan.Al.Alexandra@student.utcluj.ro>
2023-12-14 22:24:29 +0200

    Refactored to use model class

--- src/main/java/org/utcn/springproject/controllers/EventController.java
+++ src/main/java/org/utcn/springproject/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.utcn.springproject.models.Event;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,24 +15,24 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public  String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    // lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:/events";
     }
 
