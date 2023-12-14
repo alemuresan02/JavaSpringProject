b68cb2083e307cc3e673c23cb3872bc41d60b94f
alemuresan <Muresan.Al.Alexandra@student.utcluj.ro>
2023-12-14 20:56:12 +0200

    Added Dynamic Template

--- src/main/java/org/utcn/springproject/controllers/HelloController.java
+++ src/main/java/org/utcn/springproject/controllers/HelloController.java
@@ -1,27 +1,43 @@
 package org.utcn.springproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     // Responds to /hello?name=UTCN
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
+    // Responds to /hello/UTCN
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("UTCN");
+        names.add("Ale");
+        names.add("Java");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
